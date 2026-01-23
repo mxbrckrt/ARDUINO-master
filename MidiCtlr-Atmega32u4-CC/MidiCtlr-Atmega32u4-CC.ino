d70cdc6298c04ae8179f6aaad41c10f3dacf45ca
max bruckert <maxbruckert@gmail.com>
2026-01-23 17:58:19 +0100

    Cleaned the code
    Arduino Micro as midi controller needed some cleaning

--- MidiCtlr-Atmega32u4-CC/MidiCtlr-Atmega32u4-CC.ino
+++ MidiCtlr-Atmega32u4-CC/MidiCtlr-Atmega32u4-CC.ino
@@ -5,171 +5,80 @@
   This code is only for Arduinos that use ATmega32u4 (like Micro, Pro Micro, Leonardo...)
   Remember to also assign the correct board in the IDE (like Tools / Boards / Sparkfun AVR / Pro Micro...)
 ----------------
-  NOTE DE MAX BRUCKERT : 
-  1/ code modifié pour que les entrées analogiques envoies du CC, pas des notes  Se reporter à au patche [MidiCtlr-Atmega32u4-NOTES].
-    les entrées analogiques A0 à A5 == CC1 à 6 / entrees digitales 2 à 7 == CC7 à 12.
-  2/ penser à changer le nombres d'entrees analogiques. Sinon on a des parasites pourries qui font moduler toutes les entrees.
-    Editer la parties potentionmetres en fontion de nombres de capteurs connectés.
+  NOTE BY MAX BRUCKERT (Translated & Updated): 
+  1/ Removed all digital switch/button logic.
+  2/ Configured for exactly ONE analog input (A0) to send Control Change (CC) messages.
+  3/ Parasitic noise from unused pins is eliminated by reducing the input count to 1.
 */
-
-// Change any fields with //**
-
-// LIBRARY
 
 #include "MIDIUSB.h"  
 
-// BUTTONS
-const int NButtons = 6; //***  total number of buttons
-const int buttonPin[NButtons] = {2, 3, 4, 5, 6, 7}; //*** define Digital Pins connected from Buttons to Arduino; (ie {10, 16, 14, 15, 6, 7, 8, 9, 2, 3, 4, 5}; 12 buttons)
-                                            
-int buttonCState[NButtons] = {};        // stores the button current value
-int buttonPState[NButtons] = {};        // stores the button previous value
+// --- POTENTIOMETERS ---
+const int N_POTS = 1;                      // Set to 1 to read only A0
+const int POT_PIN[N_POTS] = {A0};          // Analog Pin connected to the pot/sensor
+int potCState[N_POTS] = {0};               
+int potPState[N_POTS] = {0};               
+int potVar = 0;                            
+
+int midiCState[N_POTS] = {0};              
+int midiPState[N_POTS] = {0};              
+
+const int TIMEOUT = 300;                   // Time window to consider the pot "moving"
+const int VAR_THRESHOLD = 10;              // Signal jitter threshold
+boolean potMoving = true;                  
+unsigned long PTime[N_POTS] = {0};         
+unsigned long timer[N_POTS] = {0};         
+
+// --- MIDI ASSIGNMENTS --- 
+byte midiCh = 0;        // MIDI channel (0 = Ch 1)
+byte potCCBase = 1;     // MIDI CC number to send (CC1)
 
 
-// debounce
-unsigned long lastDebounceTime[NButtons] = {0};  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    //** the debounce time; increase if the output flickers
-
-// POTENTIOMETERS
-const int NPots = 2; //*** total number of pots (knobs and faders)
-const int potPin[NPots] = {A0, A1}; //*** define Analog Pins connected from Pots to Arduino; Leave nothing in the array if 0 pots {}
-int potCState[NPots] = {0}; // Current state of the pot; delete 0 if 0 pots
-int potPState[NPots] = {0}; // Previous state of the pot; delete 0 if 0 pots
-int potVar = 0; // Difference between the current and previous state of the pot
-
-int midiCState[NPots] = {0}; // Current state of the midi value; delete 0 if 0 pots
-int midiPState[NPots] = {0}; // Previous state of the midi value; delete 0 if 0 pots
-
-const int TIMEOUT = 300; //* Amount of time the potentiometer will be read after it exceeds the varThreshold
-const int varThreshold = 10; //* Threshold for the potentiometer signal variation
-boolean potMoving = true; // If the potentiometer is moving
-unsigned long PTime[NPots] = {0}; // Previously stored time; delete 0 if 0 pots
-unsigned long timer[NPots] = {0}; // Stores the time that has elapsed since the timer was reset; delete 0 if 0 pots
-
-// MIDI Assignments 
-byte midiCh = 0; //* MIDI channel to be used -1 so it corresponds to Ch1
-byte note = 7; //* Digit inputs remapped to CC7… to follow analog ones
-byte cc = 1; //* Lowest MIDI CC to be used
-
-
-// SETUP
 void setup() {
-
-  // Baud Rate
-  // 31250 for MIDI class compliant | 115200 for Hairless MIDI
-
-  // Buttons
-  // Initialize buttons with pull up resistors
-  for (int i = 0; i < NButtons; i++) {
-    pinMode(buttonPin[i], INPUT_PULLUP);
-  }
-
+  // No digital setup required as buttons have been removed
 }
 
-////
-// LOOP
 void loop() {
-
-  buttons();
-  potentiometers();
-
+  handlePotentiometers();
 }
 
-////
-// BUTTONS
-void buttons() {
+// --- POTENTIOMETERS TO CC ---
+void handlePotentiometers() {
+  for (int i = 0; i < N_POTS; i++) {
+    potCState[i] = analogRead(POT_PIN[i]); 
+    midiCState[i] = map(potCState[i], 0, 1023, 0, 127); 
 
-  for (int i = 0; i < NButtons; i++) {
+    potVar = abs(potCState[i] - potPState[i]); 
 
-    buttonCState[i] = digitalRead(buttonPin[i]);  // read pins from arduino
-
-    if ((millis() - lastDebounceTime[i]) > debounceDelay) {
-
-      if (buttonPState[i] != buttonCState[i]) {
-        lastDebounceTime[i] = millis();
-
-        if (buttonCState[i] == LOW) {
-
-          // Sends the MIDI note ON 
-          
-         // use if using with ATmega32U4 (micro, pro micro, leonardo...)
-          controlChange(midiCh, note + i, 127);  // channel, note, velocity
-          MidiUSB.flush();
-
-
-        }
-        else {
-
-          // Sends the MIDI note OFF accordingly to the chosen board
-
-          // use if using with ATmega32U4 (micro, pro micro, leonardo...)
-          controlChange(midiCh, note + i, 0);  // channel, note, velocity
-          MidiUSB.flush();
-
-        }
-        buttonPState[i] = buttonCState[i];
-      }
-    }
-  }
-}
-
-////
-// POTENTIOMETERS
-void potentiometers() {
-
-
-  for (int i = 0; i < NPots; i++) { // Loops through all the potentiometers
-
-    potCState[i] = analogRead(potPin[i]); // reads the pins from arduino
-
-    midiCState[i] = map(potCState[i], 0, 1023, 0, 127); // Maps the reading of the potCState to a value usable in midi
-
-    potVar = abs(potCState[i] - potPState[i]); // Calculates the absolute value between the difference between the current and previous state of the pot
-
-    if (potVar > varThreshold) { // Opens the gate if the potentiometer variation is greater than the threshold
-      PTime[i] = millis(); // Stores the previous time
+    if (potVar > VAR_THRESHOLD) {
+      PTime[i] = millis(); 
     }
 
-    timer[i] = millis() - PTime[i]; // Resets the timer 11000 - 11000 = 0ms
+    timer[i] = millis() - PTime[i]; 
 
-    if (timer[i] < TIMEOUT) { // If the timer is less than the maximum allowed time it means that the potentiometer is still moving
+    if (timer[i] < TIMEOUT) {
       potMoving = true;
     }
     else {
       potMoving = false;
     }
 
-    if (potMoving == true) { // If the potentiometer is still moving, send the change control
+    if (potMoving == true) {
       if (midiPState[i] != midiCState[i]) {
-
-        // Sends  MIDI CC 
-        // Use if using with ATmega32U4 (micro, pro micro, leonardo...)
-        controlChange(midiCh, cc + i, midiCState[i]); //  (channel, CC number,  CC value)
+        // Send MIDI CC from Analog Pin
+        controlChange(midiCh, potCCBase + i, midiCState[i]); 
         MidiUSB.flush();
 
-        potPState[i] = potCState[i]; // Stores the current reading of the potentiometer to compare with the next
+        potPState[i] = potCState[i]; 
         midiPState[i] = midiCState[i];
       }
     }
   }
 }
 
-
-// if using with ATmega32U4 (micro, pro micro, leonardo...)
-
-
-// Arduino MIDI functions MIDIUSB Library
-void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOn);
-}
-
-void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOff);
-}
-
+// --- MIDI USB FUNCTIONS ---
 void controlChange(byte channel, byte control, byte value) {
+  // 0x0B = Control Change message
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
 }
